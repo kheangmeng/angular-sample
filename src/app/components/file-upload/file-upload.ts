a1d609a440884681ef75dfd946c16da375f832ab
kheangmeng <kheangmengte@gmail.com>
2026-01-15 12:41:48 +0700

    add upload file component

---
+++ src/app/components/file-upload/file-upload.ts
@@ -0,0 +1,49 @@
+import { Component, input, output, inject } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { MatButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+@Component({
+  selector: 'file-upload',
+  standalone: true,
+  imports: [MatButton, MatIconModule, MatProgressBarModule],
+  templateUrl: './file-upload.html',
+  styleUrl: './file-upload.css'
+})
+export class FileUpload {
+  private httpClient = inject(HttpClient);
+  requiredFileType = input<string>('image/*');
+  onFileUploaded = output<string>();
+
+  fileName = '';
+  uploadProgress:number| null = null;
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      this.fileName = file.name;
+      this.onFileUploaded.emit(this.fileName);
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const upload$ = this.http.post("/api/upload", formData, {
+          reportProgress: true,
+          observe: 'events'
+      })
+      .subscribe((event) => {
+        switch (event.type) {
+          case HttpEventType.UploadProgress:
+            console.log('Uploaded ' + event.loaded + ' out of ' + event.total + ' bytes');
+            break;
+          case HttpEventType.Response:
+            console.log('Finished uploading!');
+            break;
+        }
+      });
+    }
+  }
+}
