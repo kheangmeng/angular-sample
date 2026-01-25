7a41a4a9f43279fb76adc6b7a16ec45e13f50042
kheangmeng <kheangmengte@gmail.com>
2026-01-25 17:16:48 +0700

    Auto-generated commit

---
+++ chunk-MX235ZMH.js
@@ -0,0 +1,1 @@
+import{Ub as s,Vb as i,pb as o,tc as m}from"./chunk-P7EQBKWJ.js";var n=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-customer-edit"]],decls:3,vars:0,template:function(e,a){e&1&&(s(0,"div")(1,"h1"),m(2,"Update Customer"),i()())},encapsulation:2})};export{n as CustomerEdit};
